9221ae58b9e6e78455da5197da4587b007a770e4
Farah Ireen <89274209+farahirn99@users.noreply.github.com>
2024-02-21 23:52:06 +0600

    activation_function.py

--- activation_function.py
+++ activation_function.py
@@ -1,5 +1,10 @@
+import matplotlib.pyplot as plt
 import numpy as np
-
-def sigmoid(x):
-       return 1 / (1 + np.exp(-x))
+x=np.arange(-10,10)
+y=[]
+for i in range (x.shape[0]):
+    y.append((1/(1+np.exp(-x[i]))))
+plt.plot(x,y,c="blue")
+plt.title("sigmoid")
+plt.show()
 print(sigmoid(2))
